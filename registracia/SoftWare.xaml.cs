b397ea2b22635a4b40007e329c810a9e44ecb8ff
Александр Горобчук <alexandrgorobchuk@gmail.com>
2021-06-11 14:09:32 +0300

    Create Soft Ware

---
+++ registracia/SoftWare.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace registracia
+{
+    /// <summary>
+    /// Логика взаимодействия для SoftWare.xaml
+    /// </summary>
+    public partial class SoftWare : Window
+    {
+        public SoftWare()
+        {
+            InitializeComponent();
+        }
+    }
+}
